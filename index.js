1f9969dae761d13f87595ea6a79fcb8a3ebd5e41
801DevLabs <801DevLabs@gmail.com>
2019-04-19 15:09:43 -0600

    updated app response header

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ const listing = require('./routes/listing.route')
 
 const app = express()
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
