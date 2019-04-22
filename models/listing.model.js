b64468be0d579299f5f74ab2bf833148f0e95178
801DevLabs <801DevLabs@gmail.com>
2019-04-22 09:36:14 -0600

    updated listing model

--- models/listing.model.js
+++ models/listing.model.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema
 
 const ListingSchema = new Schema({
   address: {type: String, required: true, max: 100},
+  image: {type: String, required: true},
   city: {type: String, required: true},
   state: {type: String, required: true},
   style: {type: String, required: true},
